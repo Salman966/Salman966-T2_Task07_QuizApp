418fd4c639ff227388c7d99e5fcc5ff02b3eb947
Salman966 <slmanwad@gmail.com>
2025-05-26 05:53:35 +0300

    2nd commit

--- ts/main.ts
+++ ts/main.ts
@@ -4,26 +4,81 @@ import { ResultComponent } from './components/resultComponent.js';
 
 let currentQuestionIndex = 0;
 let score = 0;
+let highestScore = Number(localStorage.getItem('highestScore')) || 0;
+
+function updateHighestScore() {
+  if (score > highestScore) {
+    highestScore = score;
+    localStorage.setItem('highestScore', highestScore.toString());
+  }
+}
+
+function renderTopBar(): HTMLElement {
+  const topBar = document.createElement('div');
+  topBar.textContent = `High score: ${highestScore}`;
+  topBar.style.textAlign = 'left';
+  topBar.style.fontWeight = 'bold';
+
+  return topBar;
+}
+
 
 function showQuestion() {
   const app = document.getElementById('app');
   if (!app) return;
   app.innerHTML = '';
 
- if (currentQuestionIndex < questions.length) {
-    const current = questions[currentQuestionIndex]; 
+  const container = document.createElement('div');
+  container.appendChild(renderTopBar());
+
+  if (currentQuestionIndex < questions.length) {
+    const current = questions[currentQuestionIndex];
     const questionUI = new QuestionComponent(current, (isCorrect) => {
-      if (isCorrect) score++; 
-      currentQuestionIndex++; 
-      showQuestion(); 
+      if (isCorrect) score++;
+      currentQuestionIndex++;
+      showQuestion();
     });
 
-    app.appendChild(questionUI.render()); 
+    container.appendChild(questionUI.render());
   } else {
-    const resultUI = new ResultComponent(score, questions.length); 
-    app.appendChild(resultUI.render());
+    updateHighestScore();
+
+    const resultUI = new ResultComponent(score, questions.length);
+    container.appendChild(resultUI.render());
+
+const tryAgainBtn = document.createElement('button');
+tryAgainBtn.textContent = 'Try again';
+
+Object.assign(tryAgainBtn.style, {
+  padding: '10px 15px',
+  fontSize: '16px',
+  cursor: 'pointer',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  backgroundColor: '#f9f9f9',
+  marginTop: '20px',
+});
+
+tryAgainBtn.onmouseenter = () => {
+  tryAgainBtn.style.backgroundColor = '#00bfff';
+  tryAgainBtn.style.color = 'white';
+};
+
+tryAgainBtn.onmouseleave = () => {
+  tryAgainBtn.style.backgroundColor = '#f9f9f9';
+  tryAgainBtn.style.color = 'black';
+};
+
+tryAgainBtn.onclick = () => {
+  currentQuestionIndex = 0;
+  score = 0;
+  showQuestion();
+};
+
+    container.appendChild(tryAgainBtn);
   }
+
+  app.appendChild(container);
 }
 
 document.addEventListener('DOMContentLoaded', showQuestion);
-
